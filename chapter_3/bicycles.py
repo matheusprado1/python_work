b8d0f4117596ff06256417a1d006fa29ab0ff457
matheusprado1 <matheusprado1096@gmail.com>
2022-11-18 13:37:52 -0300

    chapter 3

--- chapter_3/bicycles.py
+++ chapter_3/bicycles.py
@@ -1,3 +1,4 @@
+# List of bicycles
 bicycles = ["trek", "cannondale", "redline", "specialized"]
 
 print(bicycles[0].title())
---
+++ chapter_3/lists.py
@@ -0,0 +1,19 @@
+names=["matheus", "joao", "angela", "adriane", "maria"]
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[-1].title())
+
+message="Welcome "+names[0].title()+"."
+print(message)
+message="Welcome "+names[1].title()+"."
+print(message)
+message="Welcome "+names[2].title()+"."
+print(message)
+message="Welcome "+names[3].title()+"."
+print(message)
+message="Welcome "+names[-1].title()+"."
+print(message)
+
