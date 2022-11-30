5bcfa4c253e9a5457c09f6c0710fd1d895d82972
matheusprado1 <matheusprado1096@gmail.com>
2022-11-30 17:16:58 -0300

    chapter 4

--- chapter_4/pizzas.py
+++ chapter_4/pizzas.py
@@ -3,3 +3,12 @@ pizzas=["portuguesa", "calabresa", "marguerita"]
 for pizza in pizzas:
     print("Gosto de pizza de " + pizza +"!")
 print("Eu realmente gosto de pizza de " + pizza + "!")
+
+pizzas.append("chocolate")
+friend_pizzas=pizzas[:]
+friend_pizzas.append("frango")
+
+for pizza in pizzas:
+    print("Gosto de pizza de " + str(pizzas))
+
+
